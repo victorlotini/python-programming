b55a14427d66ba7bef55e9c98c2fe58e87722f15
victorlotini <122792098+victorlotini@users.noreply.github.com>
2023-01-23 12:35:54 +0300

    Add files via upload

---
+++ area_of_a_circle.py
@@ -0,0 +1,10 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+pie=3.142
+def area(radius):
+    return(area)
+
+radius = int(input("Enter the radius"))
+area = pie * (radius*radius)
+
+print("area= ", area)  
